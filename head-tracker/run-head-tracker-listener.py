dc41a6e4e11404a67c69c8e72ce18a025d2d9b9e
Daniel Jones <daniel@jones.org.uk>
2025-03-26 14:31:29 +0000

    Tidy up example: head-tracker

--- head-tracker/run-head-tracker-listener.py
+++ head-tracker/run-head-tracker-listener.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 #--------------------------------------------------------------------------------
-# Head tracker example
+# SignalFlow: Head tracker example
 # Designed for the Supperware Head Tracker 1:
 # https://supperware.co.uk/headtracker-overview
+#
 #
 # Run Bridgehead, and set Profile to Yaw/Pitch/Roll (learnable)
 #--------------------------------------------------------------------------------
@@ -18,10 +19,14 @@ class BreakPlayer (Patch):
     def __init__(self, buffer_path):
         super().__init__()
         cutoff = self.add_input("cutoff", 440)
+        delay = self.add_input("delay", 0.25)
+        delay_time = self.add_input("delay_time", 0.2)
         buffer = Buffer(buffer_path)
-        player = BufferPlayer(buffer, loop=True)
+        player = BufferPlayer(buffer, loop=True) * 0.5
         filter = SVFilter(player, "low_pass", cutoff=cutoff, resonance=0.9)
-        self.set_output(filter)
+        delayed = CombDelay(filter, delay_time, feedback=0.9)
+        output = filter + delayed * delay
+        self.set_output(output)
 
 def main(args):
     graph = AudioGraph()
@@ -31,12 +36,14 @@ def main(args):
 
     def yaw_handler(_, yaw):
         print("yaw: %s" % yaw)
+        break_player.set_input("delay_time", scale_lin_lin(yaw, 0, 1, 0.01, 0.3))
     def pitch_handler(_, pitch):
         print("pitch: %s" % pitch)
-        cutoff = scale_lin_lin(pitch, 0, 1, 10, 10000)
+        cutoff = scale_lin_lin(pitch, 0, 1, 5, 10000)
         break_player.set_input("cutoff", cutoff)
     def roll_handler(_, roll):
         print("roll: %s" % roll)
+        break_player.set_input("delay", roll)
 
     dispatcher = Dispatcher()
     dispatcher.map("/yaw", yaw_handler)
@@ -44,7 +51,8 @@ def main(args):
     dispatcher.map("/roll", roll_handler)
 
     server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
-    print("Serving on {}".format(server.server_address))
+    print("Listening for OSC on {}".format(server.server_address))
+    print("Please connect a Supperware head tracker and start the Bridgehead software in Yaw/Pitch/Roll mode")
     server.serve_forever()
 
 
